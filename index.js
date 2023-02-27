2f26619132a6d139940ec9d2d1f3d408a6d2ef18
Arturo Valencia <arturovz23@gmail.com>
2023-02-26 22:04:19 -0800

    Correcion de texto en console log

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 //Comentarios en JS 
-
-console.log('Hola, este es mi primer ejercicio ocn Node en el mejor Bootcamp de programación del mundo')
+console.log('Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo')
